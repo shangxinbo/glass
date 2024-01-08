ee03a7f7bb5aafc9a0cb82034d882072a357df3a
shangxinbo <shangxinbo@163.com>
2024-01-08 21:50:23 +0800

    日志格式

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,11 +5,13 @@ module.exports = {
       port: '3000',
       exec_mode: 'cluster',
       instances: 'max',
-      script: './.output/server/index.mjs'
+      script: './.output/server/index.mjs',
+      log_date_format:'YYYY-MM-DD HH:mm:ss'
     },
     {
       name: 'cron',
-      script: './server/schedule/timing.mjs'
+      script: './server/schedule/timing.mjs',
+      log_date_format:'YYYY-MM-DD HH:mm:ss'
     }
   ]
 }
